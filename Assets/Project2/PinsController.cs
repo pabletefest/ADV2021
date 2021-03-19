c19cd9bee2b66ab8253b74c64ebf156f4687b60f
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-03-19 19:21:59 +0100

    Timeline finished

---
+++ Assets/Project2/PinsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PinsController : MonoBehaviour
+{
+    [SerializeField]
+    private List<Rigidbody> rbs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ActivatePhysicsPins", 9.23f);
+    }
+    
+    private void ActivatePhysicsPins() 
+    {
+        rbs.Select(rb => rb.isKinematic = false).ToList();
+    }
+}
---
+++ Assets/Project2/SceneReset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class SceneReset : MonoBehaviour
+{
+    [SerializeField]
+    private PlayableDirector playableDirector;
+
+    private void OnEnable()
+    {
+        playableDirector.stopped += ResetScene;
+    }
+
+    private void ResetScene(PlayableDirector obj)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void OnDisable()
+    {
+        playableDirector.stopped += ResetScene;
+    }
+}
