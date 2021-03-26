2caf2ce213c3c6792debe430c4bc3972fede5645
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-03-26 20:10:47 +0100

    Lesson 7 Portofolio 5

---
+++ Assets/00-Lesson7/AnimatorController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class AnimatorController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private string booleanParameter;
+
+    private bool toggle = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        toggle = !toggle;
+        animator.SetBool(booleanParameter, toggle);
+    }
+}
