7b01b947ff058afdf2ec68a1e50e15f3cb2d6047
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-04-13 17:45:46 +0200

    Project3 setup

---
+++ Assets/Project3/AnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project3
+{
+    [RequireComponent(typeof(Collider))]
+    public class AnimatorController : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator animator;
+
+        [SerializeField]
+        private string booleanParameter;
+
+        private bool toggle = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            toggle = !toggle;
+            animator.SetBool(booleanParameter, toggle);
+        }
+    }
+}
