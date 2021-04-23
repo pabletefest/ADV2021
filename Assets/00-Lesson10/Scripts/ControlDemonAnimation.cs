fa4e95e5adae70f86a954dc0b5a6067d0d466ae5
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-04-23 20:13:17 +0200

    Portfolio 7

---
+++ Assets/00-Lesson10/Scripts/ControlDemonAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDemonAnimation : MonoBehaviour
+{
+    private float gravityAcceleration = 9.81f;
+    private float animationConstant = 0.02f;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private CharacterController characterController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttachCharacterToGround(gravityAcceleration);
+        ResetToIdleAnim();
+        CheckUserInput();
+    }
+
+    private void CheckUserInput()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            animator.SetFloat("vertical", 1f);
+        }
+        else if (Input.GetKey(KeyCode.M))
+        {   
+            animator.SetFloat("vertical", -1f);
+        }
+        
+        if (Input.GetKey(KeyCode.L))
+        {
+            animator.SetFloat("horizontal", 1f);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            animator.SetFloat("horizontal", -1f);
+        }
+    }
+
+    private void ResetToIdleAnim()
+    {
+        animator.SetFloat("horizontal", 0);
+        animator.SetFloat("vertical", 0);
+    }
+
+    private void AttachCharacterToGround(float gravityAcceleration)
+    {
+        characterController.Move(Vector3.down * gravityAcceleration * Time.deltaTime);
+    }
+}
