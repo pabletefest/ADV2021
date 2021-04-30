8f47632588bf4d5ef81a34734a99a8b9546d3947
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-04-30 19:25:04 +0200

    Portfolio 8 ADV

---
+++ Assets/MoveAI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent playerAgent;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private Camera mainCamera;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                playerAgent.destination = hitInfo.point;
+            }
+        }
+
+        /*if (playerAgent.isOnOffMeshLink)
+        {
+            playerAnimator.SetTrigger("Jump");
+        }*/
+        
+        playerAnimator.SetFloat("horizontal", transform.InverseTransformDirection(playerAgent.velocity).x);
+        playerAnimator.SetFloat("vertical", transform.InverseTransformDirection(playerAgent.velocity).z);
+    }
+
+    // Update is called once per frame
+}
